f0ca29eefc6cf1430e5b7d6cb3bf47e2d311cbe3
Ignacio Rodrigo <irodrigo17@gmail.com>
2026-05-05 22:23:09 -0700

    feat: bind TabView selection to state in ContentView

--- HeroDirt/ContentView.swift
+++ HeroDirt/ContentView.swift
@@ -1,16 +1,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedTab = 0
+
     var body: some View {
-        TabView {
+        TabView(selection: $selectedTab) {
             MapExploreView()
                 .tabItem {
                     Label("Map", systemImage: "map")
                 }
+                .tag(0)
             SavedPlacesView()
                 .tabItem {
                     Label("Saved", systemImage: "star.fill")
                 }
+                .tag(1)
         }
     }
 }
